3eacdcb986fe00b75085a7e5ddbfb0726cf59f10
Nicholas3388 <tulip-wangwei@163.com>
2016-03-27 19:10:43 +0800

    Modified 201603271910

--- Proj318/Proj318/LoginView/Controller/LoginViewController.m
+++ Proj318/Proj318/LoginView/Controller/LoginViewController.m
@@ -10,4 +10,29 @@
 
 @implementation LoginViewController
 
+#pragma mark - load view
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self layout];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark - layout setup
+- (void)layout {
+    
+}
+
 @end
